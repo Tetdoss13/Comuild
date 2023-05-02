c9141d150d837c8bea57be364bb14b329d1211f2
erwan bauer <erwanbauer.pro@gmail.com>
2023-05-02 11:43:41 +0200

    Début Header (WIP)
    J'ai commencer le header mais il n'est pas complètement terminée.

---
+++ app/Views/header.php
@@ -0,0 +1,19 @@
+<header>
+    <nav class="menu">
+        <ul class="left">
+            <li><a href="/">Accueil</a></li>
+            <li><a href="/nos-membres">Nos Membres</a></li>
+            <li><a href="/boutique">Boutique</a></li>
+        </ul>
+        <div class="logo">
+            <a href="/">
+                <img src="../../public/img/Comuild.png">
+            </a>
+        </div>  
+        <ul class="right">
+            <li><a href="/partenaires">Partenaires</a></li>
+            <li><a href="/actualites">Actualités</a></li>
+            <li><a href="/contact">Contact</a></li>
+        </ul>
+    </nav>
+</header>
---
+++ public/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <!-- On met ici les balises meta, les tyles CSS, les scripts, etc. -->
+    </head>
+    <body>
+        <?php include('../app/Views/header.php'); ?>
+
+    </body>
+</html>
